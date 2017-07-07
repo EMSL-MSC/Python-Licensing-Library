022f6c1eb4a4b927f680bde5cb88a29c29c7cf1f
Van A. Nguyen <van.a.nguyen@pnnl.gov>
2017-07-07 15:45:33 -0700

    switched to LF line break type (previouslt CRLF), alphabetized import statements, removed unused dependencies, change args values to match POSIX convention, removed error() method, testing top level script environment before main() finction call, added additional method to prepend headers called insert_headers()

--- prepend_header.py
+++ prepend_header.py
@@ -1,20 +1,23 @@
+'''
+This program is a command-line tool that allows users to add copyright and legal notices
+to source files in software repositories
+Command line usage: prepend_header FILE DIR [options]
+'''
+
 #!/usr/bin/python
 
-import sys
-import os
 import argparse
-import fileinput
 import glob
-import fnmatch
+import os
 
 def main():
   parse()
 
 def parse():
   parser = argparse.ArgumentParser(description='Append to files in directories')
-  parser.add_argument('file', type = str, 
+  parser.add_argument('FILE', type = str, 
                   help='the header to be prepend to each source file (a plain-text file).')
-  parser.add_argument('dir', type = str,
+  parser.add_argument('DIR', type = str,
                   help='the path to the directory that contains the files to which the header will be prepended.')
   parser.add_argument('--add', action="append",
                   help='add files matching the specified glob (prefixed with the base directory) to the processing queue')
@@ -22,19 +25,33 @@ def parse():
                   help= 'remove files matching the specified glob (prefixed with the base directory) from the processing queue')
   args = parser.parse_args()
 
-  assert os.path.exists(args.file), " File path is incorrect! Couldn't find the text file at " + args.file
-  assert os.path.isdir(args.dir), 'Directory path is incorrect!'
+  assert os.path.exists(args.FILE), " File path is incorrect! Couldn't find the text file at " + args.FILE
+  assert os.path.isdir(args.DIR), 'Directory path is incorrect!'
   if (not args.add):
     print ("Please add a directory to include")
   if (args.rm):
     assert os.path.exists(args.rm), 'File path in exclude path is incorrect!'
-  select_files(args.file, args.dir, args.add, args.rm)
+  #select_files(args.FILE, args.dir, args.add, args.rm)
+  insert_headers(args.FILE, args.DIR, args.add, args.rm)
 
 def select_files(textfile, directory, include_files, exclude_files):
-  count = 0
   acc = []
-  for x in os.walk(directory):
-    print x[0]
+  #  for x in os.walk(directory):
+  #   print x[0]
+  # for item in include_files:
+  #   for name in glob.glob(item):
+  #     count = count + 1
+  #     print (str(count) + (name))
+
+'''More optimized solution to add headers to file'''
+
+def insert_headers(textfile, directory, include_files, exclude_files):
+  with open(textfile, 'r') as original:data1 = original.read()
+  with open('test.txt', 'r') as modified:data2 = modified.read()
+  with open('test.txt', 'w') as original:data = original.write(data1 + "\n" + data2)
+
+'''Append headers by first copying header to a temp tile, copy the content 
+of the file that header is being appended to'''
 
 def add_headers(textfile, directory, include_files, exclude_files):
   with open(textfile) as f:
@@ -44,16 +61,11 @@ def add_headers(textfile, directory, include_files, exclude_files):
           f1.write(line)
         f.close()
         f1.write("\n\n") 
-        with open("admin.txt") as f2:
+        with open("test.txt") as f2:
           for line in f2:
            f1.write(line)
           f1.close()
         f2.close()
 
-def error():
-    print ("-----------------------------------------------------------------------")
-    print("Incorrect number of arguments")
-    print ("Usage: prepend_header.py FILE DIR [--include=GLOB]* [--exclude=GLOB]*")
-
-
-main()
+if __name__ == "__main__":
+  main()
