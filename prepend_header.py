2a65cab04b2dba0bef4c05e3f33457dc5c706fb8
Van A. Nguyen <van.a.nguyen@pnnl.gov>
2017-07-12 16:10:01 -0700

    identified and converted path in UNIX-style using regex, automated the replacement of path/to/file string in header with project path, added --path option to replace template path with project path

--- prepend_header.py
+++ prepend_header.py
@@ -10,6 +10,7 @@ import argparse
 import glob2
 import logging
 import os
+import re
 
 def main():
   verbosity_setup()
@@ -21,24 +22,49 @@ def parse():
                   help = 'the header to be prepend to each source file (a plain-text file).')
   parser.add_argument('DIR', type = str, 
                   help = 'the path to the directory that contains the files to which the header will be prepended.')
-  parser.add_argument('--add', action = "append", 
+  parser.add_argument('--add', action = 'append', 
                   help = 'add files matching the specified glob (prefixed with the base directory) to the processing queue')
-  parser.add_argument('--rm', action = "append", 
+  parser.add_argument('--rm', action = 'append', 
                   help = 'remove files matching the specified glob (prefixed with the base directory) from the processing queue')
-  parser.add_argument('--verbose', action = "store_true", 
-                  help = "raise the verbosity level (log debug and information messages to the standard error stream)")
+  parser.add_argument('--path', type = str, 
+                  help = 'replacing template path with actual project path')
+  parser.add_argument('--verbose', action = 'store_true', 
+                  help = 'raise the verbosity level (log debug and information messages to the standard error stream)')
   parser.add_argument('--version', action = 'version', version = '%(prog)s version 1.0', 
-                  help = "display the version")
+                  help = 'display the version')
   args = parser.parse_args()
 
-  assert os.path.exists(args.FILE), "File path is incorrect! Couldn't find the text file at " + args.FILE
+  assert os.path.exists(args.FILE), 'File path is incorrect! Could not find the text file at ' + args.FILE
   assert os.path.isdir(args.DIR), 'Directory path is incorrect!'
   if (not args.add):
     logging.error('--add field is required')
   elif (args.add):
     acc = process_files(args.DIR, args.add, args.rm)
     execute(args.FILE, args.DIR, acc, args.verbose, args)
+    identify_project_paths(args.DIR, acc, args.path)
     return 0
+
+def identify_project_paths(directory, acc_list, path):
+  files = {}
+  for i in acc_list:
+    files[i] = i.lstrip(directory)
+  if (os.name == 'nt'): #check if running on Windows environment
+    files = convert_paths(files)
+  append_paths_to_files(files, path)
+
+def append_paths_to_files(files, path):
+  for key, value in files.items():
+    s = open(key).read()
+    s = s.replace(str(path), value)
+    f = open(key, 'w')
+    f.write(s)
+    f.close()
+
+def convert_paths(files):
+  for key, value in files.items():
+    x = re.sub('\\\\', '/', value)
+    files[key] = x
+  return files
 
 def process_files(directory, include_files, exclude_files):
   include_list = include(directory, include_files)
@@ -101,5 +127,5 @@ def insert_headers(textfile, append_list, verbosity):
     if (verbosity):
       print(filename)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
