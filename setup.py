90d10eb178c09ee760e6f996774824bfb2afac9c
Mark Borkum <mark.borkum@pnnl.gov>
2017-08-21 08:42:32 -0700

    Added LICENSE, NOTICE and WARRANTY non-source files to setup script.

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# Python-Licensing-Library: setup.py
-# Copyright (c) 2017 Pacific Northwest National Laboratory.
-# See LICENSE for details.
-
 #!/usr/bin/env python
 
 from distutils.core import setup
@@ -9,22 +5,22 @@ from distutils.core import setup
 setup(
   name = 'python-licensing-library',
   version = '1.0',
-  description = 'Prepend headers to source files',
-  license = 'Pacific Northwest National Laboratory',
-  author = 'Van Nguyen',
+  description = 'Prepend headers to selected source files.',
+  license = 'ECL-2.0',
+  author = 'Van A. Nguyen',
   author_email = 'van.a.nguyen@pnnl.gov',
   packages = ['python-licensing-library'],
-  long_description = open('README.txt').read(),
-  package_data = {'':['requirements.txt']},
-  data_files = [('', ['LICENSE.txt'])],
+  long_description = open('README.md').read(),
+  package_data = {'': ['requirements.txt']},
+  data_files = [('', ['LICENSE', 'NOTICE', 'WARRANTY'])],
   include_package_data = True,
   install_requires = [
-      'glob2==0.5',
-  ], 
+    'glob2==0.5',
+  ],
   classifiers = [
-      'Intended Audience :: PNNL Software Developers',
-      'Programming Language :: Python ::3',
-      'Topic :: Software Development :: Libraries',
-      'License :: Educational Community License 2.0: Pacific Northwest National Laboratory'
-  ]
+    'License :: ECL-2.0',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries',
+  ],
 )
